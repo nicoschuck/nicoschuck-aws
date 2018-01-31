a78dbe149c0513f2ac74c98723037f39720f6d52
nicoschuck <nicoschuck7@googlemail.com>
2018-01-31 15:29:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var express = require('express');
+var express 			= require('express');
+
+var session 			= require('express-session');
+
+var passport 			= require('passport');
+var lokcalStrategy 		= require('passport-local').Strategy;
+var dbUser 				= require('./db/index.ejs');
+var mongoose 			= require('mongoose');
+var bodyParser 			= require('body-parser');
+
+
+var MongoClient 		= require('mongodb').MongoClient;
+var server 				= require('http').Server(app);
+var socketio 			= require('socket.io')(server)
 var port = process.env.PORT || 3000;
 var app = express(),
 path = require('path'),
